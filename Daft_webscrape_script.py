edb2ff2357824fb0e74e5f3c6399b344f9d2e96a
Martin Coffey <mcoffey1129@hotmail.com>
2021-06-27 23:36:38 +0100

    Update to the code - 6th save

--- Daft_webscrape_script.py
+++ Daft_webscrape_script.py
@@ -97,18 +97,12 @@ print(len(btype_lst))
 
 #
 loc_df = pd.DataFrame(loc_lst,columns = ['address'])
-
-
-county_lst= ['Co.Antrim','Co.Armagh','Co.Carlow','Co.Cavan','Co.Clare','Co.Cork','Co.Donegal','Co.Down','Co.Dublin',
-'Co.Fermanagh','Co.Galway','Co.Kerry','Co.Kildare','Co.Kilkenny','Co.Laois','Co.Leitrim','Co.Limerick','Co.Derry',
-'Co.Longford','Co.Louth','Co.Mayo','Co.Meath','Co.Monaghan','Co.Offaly','Co.Roscommon','Co.Sligo','Co.Tipperary',
-'Co.Tyrone','Co.Waterford','Co.Westmeath','Co.Wexford','Co.Wicklow']
-
 loc_df['county'] =loc_df['address'].str.extract('(Co. [A-Z][a-z]+)',expand=True)
 loc_df.head()
 
 pc_df = pd.DataFrame(price_lst,columns = ['ref', 'price'])
 pc_df =pc_df.loc[pc_df['ref'].str[:9]=='/for-sale']
+
 
 bba_df =pd.DataFrame(bba_lst,columns=['bba'])
 bba_wrk = bba_df['bba'].str.split(pat='·',expand=True)
@@ -144,73 +138,39 @@ bba_wrk.loc[bba_wrk['bed_t'].isin(prop_lst), ['prop_type']] = bba_wrk['bed_t']
 bba_wrk.loc[bba_wrk['bath_t'].isin(prop_lst) , ['prop_type']] = bba_wrk['bath_t']
 bba_wrk.loc[bba_wrk['area_t'].isin(prop_lst) , ['prop_type']] = bba_wrk['area_t']
 bba_wrk.loc[bba_wrk['prop_type_t'].isin(prop_lst), ['prop_type']] = bba_wrk['prop_type_t']
+bba_wrk.head()
 
+bba_df_final =  bba_wrk.drop(columns=['bed_t','bath_t','area_t','prop_type_t', 'extra_t'])
+bba_df_final.fillna('', inplace=True)
+bba_df_final.head()
 
+daft_df_1 = pd.concat([loc_df, bba_df_final, pc_df], axis=1)
+daft_df1_1.head()
 
-bba_wrk[]
+corr_df = pd.concat([pd.DataFrame(col_list, columns=['columns']), pd.DataFrame(corr_list
+                                                                               , columns=['Correlation'])], axis=1)
 
-bba_wrk.to_csv(r'Files\bba_wrk.csv', index=False, header=True)
-
-loc_df.shape
-pc_df.shape
-bba_df.head()
-
-bba_wrk.head(10)
-
-daft_df = pd.concat
-
-bba_wrk.to_csv(r'Files\bba_wrk.csv', index=False, header=True)
+# bba_wrk.to_csv(r'Files\bba_wrk.csv', index=False, header=True)
+#
+# loc_df.shape
+# pc_df.shape
+# bba_df.head()
+#
+# bba_wrk.head(10)
+#
+# daft_df = pd.concat
+#
+# bba_wrk.to_csv(r'Files\bba_wrk.csv', index=False, header=True)
 
 ################################################################################
 
-req_daft2 = requests.get('https://www.daft.ie/property-for-sale/ireland?from=20&pageSize=20')
-req_daft2.text # shows the html code as is which is useless at the moment
-
-https://www.daft.ie/property-for-sale/ireland?pageSize=20&from=40
-https://www.daft.ie/property-for-sale/ireland?pageSize=20&from=60
-https://www.daft.ie/property-for-sale/ireland?pageSize=20&from=80
-https://www.daft.ie/property-for-sale/ireland?pageSize=20&from=100
 
 
-# Create a beautiful soup object search which will be used to parse the daft.ie website
-search2 = bs(req_daft2.content, 'html.parser')
-print(type(search2))
 
 # Use the search object to extract the information required from the html code
 # For location it will search the html code where class = 'TitleBlock__Address-sc-1avkvav-7'
 # Please note we cut off the last few digits in the class in order to bring in the
 # featured property
-
-loc_bs = soup.findAll("p", {'class': 'TitleBlock__Address-sc-1avkvav-7 knPImU'})
-bed_bath_area_bs = soup.findAll("div",{'class': 'TitleBlock__CardInfo-sc-1avkvav-9 QvaxK'})
-btype_bs = soup.findAll("p", {'class': 'TitleBlock__CardInfoItem-sc-1avkvav-8 bcaKbv'})
-price_span_bs = soup.findAll("span", {'class': 'TitleBlock__StyledSpan-sc-1avkvav-4 gDBFnc'})
-
-# Location and building type do not pick up the "featured value properties" while
-# bed/bath/area and price do, however these values are incorrect and need to be deleted
-
-price_lst = []
-bba_lst = []
-for i in range(len(price_span_bs)):
-    pc_txt_i = price_span_bs[i].text
-    price_lst.append((pc_parent_txt_i, pc_txt_i))
-
-    bba_txt_i = bed_bath_area_bs[i].text
-    if len(bba_txt_i) > 0:
-        bba_lst.append(bba_txt_i)
-print(len(price_lst))
-print(len(bba_lst))
-
-loc_lst = []
-btype_lst = []
-for i in range(len(loc_bs)):
-    loc_txt_i = loc_bs[i].text
-    btype_txt_i = btype_bs[i].text
-    loc_lst.append(loc_txt_i)
-    btype_lst.append(btype_txt_i)
-print(len(loc_lst))
-print(len(btype_lst))
-
 
 
 loc_bs_sp = search2.findAll("p", {'class': 'TitleBlock__Address-sc-1avkvav-7 eARcqq'})
